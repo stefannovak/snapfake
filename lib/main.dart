f931a66f51265b1f9630cc6e6054cf09960a3cb6
Stefan Novak <stefannovak96@gmail.com>
2020-08-13 11:43:25 +0100

    Part of the inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Snapfake',
       theme: ThemeData(
         // This is the theme of your application.
         //
