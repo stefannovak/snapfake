61cedba2cfed9f461516f1a58d5898edb9b06c32
Stefan Novak <stefannovak96@gmail.com>
2020-08-18 20:50:24 +0100

    Pre-Firebase Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:snapfake/screens/app/camera_screen.dart';
 import 'package:snapfake/screens/app/chatlist_screen.dart';
@@ -6,11 +7,41 @@ import 'package:snapfake/screens/app/story_screen.dart';
 import 'package:snapfake/screens/home/home_screen.dart';
 import 'package:snapfake/screens/home/registration_screen.dart';
 
-void main() {
-  runApp(MyApp());
+//void main() {
+//  runApp(MyApp());
+//}
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+        CameraScreen.id: (context) => CameraScreen(camera: firstCamera),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatListScreen.id: (context) => ChatListScreen(),
+        StoryScreen.id: (context) => StoryScreen(),
+        MyPageView.id: (context) => MyPageView(),
+      },
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+//  final CameraDescription cameraDescription;
+//
+//  MyApp(this.cameraDescription);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/app/camera_screen.dart
+++ lib/screens/app/camera_screen.dart
@@ -1,17 +1,106 @@
-import 'package:flutter/material.dart';
+//import 'package:camera/camera.dart';
+//import 'package:flutter/material.dart';
+//import 'package:path/path.dart';
+//import 'package:path_provider/path_provider.dart';
 
-class CameraScreen extends StatelessWidget {
+import 'dart:async';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+import 'display_picture_screen.dart';
+
+class CameraScreen extends StatefulWidget {
   static String id = "camera_screen";
+  final CameraDescription camera;
+
+  CameraScreen({this.camera});
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  ///MOST OF THIS SHOULD BE A BLOC
+  // Add two variables to the state class to store the CameraController and
+  // the Future.
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.ultraHigh,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("CAMERA SCREEN"),
-        ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the path
+            // package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
